193d29124166b1e6dd4703fd632eb6624b7cdcca
Leekwangsoo1995 <jayzisgodminion@gmail.com>
2020-12-27 14:54:50 +0900

    selection sort is not stable, bubble sort is stable

--- sorts/insertion_sort.cc
+++ sorts/insertion_sort.cc
@@ -1,44 +1,53 @@
 #include <iostream>
 using namespace std;
 
-int selectionSort(int A[],int N){
-    int sw = 0;
-    bool flag = 0;
+void selectionSort(int A[],int N){
     for(int i=0;i<N;i++){
         int minx = i;
         for(int j=i; j<N;j++){
             if(A[j] < A[minx]){
                 minx = j;
-                flag = true;
             }
         }
-        if(flag == true){
-            sw++;
-            swap(A[i],A[minx]);
-            flag = false;
+        swap(A[i],A[minx]);
+    }
+}
+
+void bubbleSort(int A[],int N){
+    for(int i=0;i<N;i++){
+        for(int j=N;j>i;j--){
+            if(A[j] < A[j-1]){
+                swap(A[j],A[j-1]);
+            }
         }
     }
-    return sw;
-
 }
 
 
 int main(void){
-	int A[100],N,sw;
+	int A[100],B[100],N;
 
 	std::cout << "total = ";
     cin >> N;
 	std::cout << "nums = ";
     for(int i=0; i<N; i++)cin >> A[i];
+    for(int i=0; i<N; i++)B[i] = A[i];
+    
 
-    sw = selectionSort(A,N);
+    selectionSort(A,N);
+    bubbleSort(B,N);
 
     for(int i = 0;i<N;i++){
         if(i) cout << " ";
         cout << A[i]; 
     }
 	std::cout << endl;
-	std::cout << sw << endl;
+
+    for(int i = 0;i<N;i++){
+        if(i) cout << " ";
+        cout << B[i]; 
+    }
+	std::cout << endl;
 
     return 0;
 }
