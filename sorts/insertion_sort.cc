d0df6db4cfabcff2532fefda17756a96a029f5b6
Leekwangsoo1995 <jayzisgodminion@gmail.com>
2020-12-28 14:28:12 +0900

    ポインタ完全に理解した

--- sorts/insertion_sort.cc
+++ sorts/insertion_sort.cc
@@ -16,29 +16,90 @@ void init(){
     nil->prev = nil;
 }
 
+void insert(int key){
+    Node *x = (Node *)malloc(sizeof(Node));
+    x->key = key;
+    //番兵の直後に要素を追加いたしやす
+    x->next = nil->next;
+    std::cout << nil << endl;
+    std::cout << nil->next << endl;
+    std::cout << nil->next->prev << endl;
+    nil->next->prev = x;
+    nil->next = x;
+    x->prev = nil;
+}
+
+Node* listSearch(int key){
+    Node *cur = nil->next;
+    while(cur != nil && cur->key != key){
+        cur = cur->next;
+    }
+    return cur;
+}
+
+void deleteNode(Node *t){
+    if(t == nil) return;
+    t->prev->next = t->next;
+    t->next->prev = t->prev;
+    free(t);
+}
+
+void deleteFirst(){
+    deleteNode(nil->next);
+}
+
+void printList(){
+    Node *cur = nil->next;
+    int isf = 0;
+    while(1){
+        if(cur == nil)break;
+        if(isf++ > 0)printf(" ");
+        printf("%d",cur->key);
+        cur = cur->next;
+    }
+    printf("¥n");
+}
+
+void deleteLast(){
+    deleteNode(nil->prev);
+}
+
+void deleteKey(int key){
+    deleteNode(listSearch(key));
+}
+
 int main(){
     int key,n,i;
     int size = 0;
     char com[20];
-    int coms[20];
     int np=0,nd=0;
     scanf("%d",&n);
     init();
 
     for(i=0;i<n;i++){
-        scanf("%s%d",com,coms);
-	    std::cout << com[0] << endl;
-	    std::cout << coms[0] << endl;
-	    std::cout << &key << endl;
+        scanf("%s%d",com,&key);
         if(com[0] == 'i'){
-
+            insert(key);
+            np++;
+            size++;
+        }else if(com[0] == 'd'){
+            if(strlen(com)>6){
+                if(com[6] == 'F')deleteFirst();
+                else if(com[6]== 'L')deleteLast();
+            }else{
+                deleteKey(key);nd++;
+            }
+            size--;
         }
     }
 
+    printList();
+    return 0;
+    
     std::cout << endl;
-	std::cout << nil << endl;
+	/*std::cout << nil << endl;
 	std::cout << nil->next << endl;
-	std::cout << nil->prev << endl;
+	std::cout << nil->prev << endl;*/
 
 }
 
