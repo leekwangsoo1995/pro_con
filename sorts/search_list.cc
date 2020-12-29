492281ca05f36bc61377b56a6aec59233a7cf561
Leekwangsoo1995 <jayzisgodminion@gmail.com>
2020-12-29 15:57:11 +0900

    binary tree

--- sorts/search_list.cc
+++ sorts/search_list.cc
@@ -1,20 +1,37 @@
 #include<iostream>
 using namespace std;
+int N,M,A[100],B[100],flag;
+
+int binarySearch(int key){
+    int left,right,mid;
+    left = 0;
+    right = N;
+    while (left < right){
+        mid = (right + left) / 2;
+        if(A[mid] == key){
+            return mid;
+        }
+        if(A[mid] > key){
+            right = mid;
+        }else{
+            left = mid + 1;
+        }
+    }
+    return 0;
+
+
+}
 
 int main(){
-    int N,M,A[100],B[100],flag;
+    int result;
     flag=0;
     cin >>N;
     for(int i=0;i<N;i++)cin>>A[i];
     cin >>M;
     for(int i=0;i<M;i++)cin>>B[i];
-
     for(int i=0;i<M;i++){
-        for(int j=0;j<N;j++){
-            if(B[i] == A[j]){
-                flag++;
-            }
-        }
+        result = binarySearch(B[i]);
+        cout << result << endl;
     }
-    cout << flag;
+
 }
