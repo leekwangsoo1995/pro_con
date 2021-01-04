529a731a4e376b39bdab044c9603c1dd1f198fc2
Leekwangsoo1995 <jayzisgodminion@gmail.com>
2021-01-04 23:13:17 +0900

    二分探索学習中...

--- sorts/tree.cc
+++ sorts/tree.cc
@@ -1,61 +1,34 @@
-#include<iostream>
-using namespace std;
-#define MAX 100005
+#include<cstdio>
+#define MAX 10000
 #define NIL -1
 
-struct Node{int p,l,r;};
+struct Node {int parent,left,right;};
 
 Node T[MAX];
-int n,D[MAX];
-void print(int u){
-    int i,c;
-    cout << "node " << u << ": ";
-    cout << "parent = " << T[u].p << ", ";
-    cout << "depth = " << D[u] << ",";
-
-    if( T[u].p == NIL)cout << "root, ";
-    else if(T[u].l == NIL)cout << "leaf, ";
-    else cout << "internal code, ";
-
-    cout << "[";
-
-    for(i = 0,c = T[u].l;c != NIL;i++,c = T[c].r){
-        if(i)cout << ", ";
-        cout << c;
-    }
-    cout << "]" << endl;
-}
-
-//再帰的に深さを求める
-int rec(int u,int p){
-    D[u] = p;
-    if( T[u].r != NIL ) rec(T[u].r,p);//右の兄弟に同じ深さ設定
-    if( T[u].l != NIL ) rec(T[u].l,p+1);//最も左の子に自分の深さ+1を追加
-}
+int n,D[MAX],H[MAX];
 
 int main(){
-    int i,j,d,v,c,l,r;
-    cin >> n;
-    for(i = 0;i < n; i++) T[i].p = T[i].l = T[i].r = NIL;
+    int v,l,r,root=0;
+    scanf("%d",&n);
 
-    for(i = 0;i<n;i++){
-        cin >> v >> d;
-        for( j = 0; j < d; j++){
-            cin >> c;
-            if( j == 0) T[v].l = c;
-            else T[l].r = c;
-            l = c;
-            T[c].p = v;
-        }
-    }
-    for(i=0;i<n;i++){
-        if(T[i].p == NIL) r=i;
+    for(int i=0;i<n;i++)T[i].parent = NIL;
+
+    for(int i=0;i<n;i++){
+        scanf("%d %d %d",&v,&l,&r);
+        T[v].left = l;
+        T[v].right = r;
+        if( l != NIL )T[l].parent = v;
+        if( r != NIL )T[r].parent = v;
     }
 
-    rec(r,0);
+    for(int i = 0;i < n;i++ )if(T[i].parent == NIL)root = i;
 
-    for(i=0;i<n;i++)print(i);
+    //setDepth(root,o);
+    //setHeight(root);
+    
+    //for(int i=0;i<n;i++)print(i);
 
     return 0;
 }
+
 
