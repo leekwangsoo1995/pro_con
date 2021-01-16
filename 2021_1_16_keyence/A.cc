af0ad1398ddd2f9e4e64da23e0b682170b817f95
Leekwangsoo1995 <jayzisgodminion@gmail.com>
2021-01-16 22:50:15 +0900

    修正

--- 2021_1_16_keyence/A.cc
+++ 2021_1_16_keyence/A.cc
@@ -3,7 +3,7 @@
 using namespace std;
 //https://atcoder.jp/contests/keyence2021/tasks/keyence2021_a
 //間違いその１、C++のint型の最大値は2147483647である。それ以上の数値は保存できない。
-//実行時の計算量が物凄い事になっている。あとで何がダメだったかしっかり見直しを行う。
+//風呂上りに計算量を少なくして挑戦してみたけどやっぱりダメ。だった。
 
 int n;
 
@@ -11,9 +11,11 @@ int main(){
     cin >> n;
     int max = n*n;
     
-    long long number[max];
-    long long a[n];
-    long long b[n];
+    unsigned long long number[max];
+    unsigned long long a[n];
+    unsigned long long b[n];
+    unsigned long long biga=0;
+    unsigned long long bigb=0;
 
     for(int i=0;i<n;i++){
         cin >> a[i];
@@ -26,11 +28,18 @@ int main(){
     for(int i=0;i<n;i++){
        number[i] = 0;
        for(int x=0;x<i+1;x++){
+           if(biga < a[x]){
+               biga = a[x];
+           }
+           bigb=0;
            for(int k=0;k<i+1;k++){
                if(x>k){
                   continue;
                }
-               if(number[i] < a[x]*b[k]){
+               if(bigb < b[k]){
+                bigb = b[k];
+               }
+               if(a[x] == biga && b[k] == bigb && number[i] < a[x]*b[k]){
                    number[i] = a[x]*b[k];
                }
            }
