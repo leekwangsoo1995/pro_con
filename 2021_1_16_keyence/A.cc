4a68adc6d09754f3d25b41c4087d41aedf9a13db
Leekwangsoo1995 <jayzisgodminion@gmail.com>
2021-01-17 10:44:19 +0900

    keyence_解答

--- 2021_1_16_keyence/A.cc
+++ 2021_1_16_keyence/A.cc
@@ -43,3 +43,33 @@ int main(){
 
     return 0;
 }
+
+//解答。
+//MAX関数の使い方を理解していれば、かなり簡単に書ける。
+//Aは最大値を常に保持しておく。
+//Bは常に最新の値で掛け算しておく
+//A*Bで出したそれまでの積と最新の掛けた値を比較する事で、解答が得られる。
+//プロコンの問題は、問題を解くまえに、どうすれば計算を最短でできるか考えた方が良さそう。
+//計算した方が良い部分と、計算しなくて良い部分を事前に考えておく事。
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using ll = long long;
+using namespace std;
+const char EOLN = '\n';
+int n;
+int main() {
+  cin >> n;
+  vector<ll> a(n), b(n), c(n);
+  for (int i = 0; i < n; i++) cin >> a[i];
+  for (int i = 0; i < n; i++) cin >> b[i];
+  ll ma = a[0];
+  c[0] = a[0] * b[0];
+  for (int i = 1; i < n; i++) {
+    ma = max(ma, a[i]);
+    c[i] = max(c[i - 1], ma * b[i]);
+  }
+  for (int i = 0; i < n; i++) cout << c[i] << endl;
+  return 0;
+}
