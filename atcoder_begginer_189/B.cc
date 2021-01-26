861475c57461fd24522ee308000974fca010eed0
Leekwangsoo1995 <jayzisgodminion@gmail.com>
2021-01-26 21:41:42 +0900

    丸め誤差に嵌められていた件

--- atcoder_begginer_189/B.cc
+++ atcoder_begginer_189/B.cc
@@ -1,17 +1,20 @@
 #include<iostream>
 using namespace std;
 
+//原因：小数点以下の丸め誤差の影響によって、不正解になっていた。
+//小数点以下の計算がある時は、100倍数するなど、丸め誤差を避ける方が良い。
 
 int n,x;
 int num[1000];
 int per[1000];
-double amount;
-double tot_amount;
+int amount;
+int tot_amount;
 
 int main(){
     cin >> n;
     cin >> x;
 
+    x = x * 100;
     amount = 0;
     tot_amount = 0;
     for(int i=0;i<n;i++){
@@ -23,10 +26,10 @@ int main(){
         if(per[i] == 0){
             amount = 0;
         }else{
-            amount = (double)num[i] * ((double)per[i] / 100);
+            amount = num[i] * per[i];
         }
-        tot_amount = (double)amount + tot_amount;
-        if((double)x < tot_amount){
+        tot_amount = amount + tot_amount;
+        if(x < tot_amount){
             i = i +1; 
             cout << i;
             return 0;
